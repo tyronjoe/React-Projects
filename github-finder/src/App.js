856d6789d7cd4b62ab671a4b37fe47c5eb82adfa
Tyron Joe <tyronjoe55@gmail.com>
2020-06-09 18:28:44 +0200

    first day

---
+++ github-finder/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const name = "Barry Allen";
+    const loading = false;
+    const showName = true;
+    return (
+      <div className='App'>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+      </div>
+    );
+  }
+}
+export default App;
