0eb3e754e3967ce8c67d81db040be1ed9fddb285
Tyron Joe <tyronjoe55@gmail.com>
2020-06-11 00:44:10 +0200

    day 2

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
-    const name = "Barry Allen";
-    const loading = false;
-    const showName = true;
     return (
       <div className='App'>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
