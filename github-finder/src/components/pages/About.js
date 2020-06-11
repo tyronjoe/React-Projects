c3c6e6140ba2cca8fbaf613133b518c7fd061f51
Tyron Joe <tyronjoe55@gmail.com>
2020-06-11 21:53:27 +0200

    day 3

---
+++ github-finder/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>An app to search for Github users</p>
+      <p>Version: Latest</p>
+    </Fragment>
+  );
+};
+
+export default About;
