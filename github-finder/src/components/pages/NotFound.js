c37773e6d13a3ec320b5bf1937821874f5da57d1
Tyron Joe <tyronjoe55@gmail.com>
2020-06-12 18:21:26 +0200

    Final day no Netlify

---
+++ github-finder/src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className='lead'>
+        The page you are looking for is somewhere else. Scram...
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
