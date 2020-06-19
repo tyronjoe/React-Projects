4774ce4250facd19162d18cdd6ddbb5a3d32a3dd
Tyron Joe <tyronjoe55@gmail.com>
2020-06-19 12:02:00 +0200

    day 2

---
+++ contact-finder/client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        This is a full stack React App for keeping contacts
+      </p>
+      <p className='bg-dark p'>
+        <b>Version: </b> Latest
+      </p>
+    </div>
+  );
+};
+
+export default Home;
